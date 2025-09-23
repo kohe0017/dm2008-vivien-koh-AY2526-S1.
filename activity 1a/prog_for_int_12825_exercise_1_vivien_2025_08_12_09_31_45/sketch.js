745a11b1f706841ee7932987b6224dee87b1a6cb
kohe0017 <kohe0017@e.ntu.edu.sg>
2025-09-23 11:25:50 +0800

    Activity 1a

---
+++ activity 1a/prog_for_int_12825_exercise_1_vivien_2025_08_12_09_31_45/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(1000, 600);
+  angleMode (RADIANS)
+}
+
+function draw() {
+  background("#9E73E9");
+  fill('#000000')
+  strokeWeight (4)
+  ellipse(400,200,150)
+  ellipse (330, 150, 40)
+   ellipse (470, 150, 40)
+   ellipse (400, 350, 150, 200)
+   ellipse (300, 290, 120, 40)
+   ellipse (300, 430, 120, 40)
+  ellipse (500, 430, 120, 40)
+    ellipse (500, 290, 120, 40)
+  fill('')
+   ellipse (400, 220, 20)
+  
+fill('#265')
+   ellipse (100, 100, 75, 100)
+   ellipse (150, 145, 60, 20)
+   ellipse (155, 60, 60, 20)
+  ellipse (50, 145, 60, 20)
+    ellipse (45, 60, 60, 20)
+    ellipse (100, 35, 30, 30)
+  line (100,50, 100, 145)
+   line (80,60, 80, 145)
+     line (120,60, 120, 145)
+     line (65, 90, 135, 90)
+       line (65, 120, 135, 120)
+  
+  
+       
+rotate (QUARTER_PI)
+  ellipse (200, 200, 135, 90);
+
+}
+
